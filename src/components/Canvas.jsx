5d7d4365ca4a7667a43eced3cf7af3fb73a35dd5
Robert Firstman <robfirstman@gmail.com>
2018-12-18 11:50:53 -0500

    Part 2: Finished--added animations, title, and start game functionality

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -8,9 +8,13 @@ import CannonPipe from './CannonPipe';
 import CannonBall from "./CannonBall";
 import CurrentScore from "./CurrentScore";
 import FlyingObject from "./FlyingObject";
+import Heart from "./Heart";
+import StartGameButton from "./StartGameButton";
+import Title from "./Title";
 
 const Canvas = (props) => {
-    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+    const gameHeight = 1200;
+    const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
     return (
         <svg
             id="aliens-go-home-canvas"
@@ -20,24 +24,48 @@ const Canvas = (props) => {
         >
             <defs>
                 <filter id="shadow">
-                    <feDropShadow dx="1" dy="1" stdDeviation="2"/>
+                    <feDropShadow dx="1" dy="1" stdDeviation="2" />
                 </filter>
             </defs>
             <Sky />
             <Ground />
             <CannonPipe rotation={props.angle} />
             <CannonBase />
-            <CannonBall position={{x: 0, y: -100}}/>
             <CurrentScore score={15} />
-            <FlyingObject position={{x: -150, y: -300}}/>
-            <FlyingObject position={{x: 150, y: -300}}/>
+
+            {!props.gameState.started &&
+                <g>
+                    <StartGameButton onClick={() => props.startGame()} />
+                    <Title />
+                </g>
+            }
+
+            {props.gameState.flyingObjects.map(flyingObject => (
+                <FlyingObject
+                    key={flyingObject.id}
+                    position={flyingObject.position}
+                />
+            ))}
         </svg>
     );
 };
 
 Canvas.propTypes = {
-  angle: PropTypes.number.isRequired,
-  trackMouse: PropTypes.func.isRequired,
+    angle: PropTypes.number.isRequired,
+    gameState: PropTypes.shape({
+        started: PropTypes.bool.isRequired,
+        kills: PropTypes.number.isRequired,
+        lives: PropTypes.number.isRequired,
+        flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+            position: PropTypes.shape({
+                x: PropTypes.number.isRequired,
+                y: PropTypes.number.isRequired
+            }).isRequired,
+            id: PropTypes.number.isRequired,
+        })).isRequired,
+    }).isRequired,
+    trackMouse: PropTypes.func.isRequired,
+    startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;
--- src/components/FlyingObject.jsx
+++ src/components/FlyingObject.jsx
@@ -1,21 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+import FlyingObjectBase from './FlyingObjectBase';
+import FlyingObjectTop from './FlyingObjectTop';
+import { gameHeight } from '../utils/constants';
 
-import FlyingObjectBase from "./FlyingObjectBase";
-import FlyingObjectTop from "./FlyingObjectTop";
+const moveVertically = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(${gameHeight}px);
+  }
+`;
 
-const FlyingObject = (props) => (
-    <g>
+const Move = styled.g`
+  animation: ${moveVertically} 4s linear;
+`;
+
+const FlyingObject = props => (
+    <Move>
         <FlyingObjectBase position={props.position} />
         <FlyingObjectTop position={props.position} />
-    </g>
+    </Move>
 );
 
 FlyingObject.propTypes = {
     position: PropTypes.shape({
         x: PropTypes.number.isRequired,
         y: PropTypes.number.isRequired
-    }).isRequired
+    }).isRequired,
 };
 
 export default FlyingObject;
